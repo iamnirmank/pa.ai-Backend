63df723fe05dac4e76812b525ea25a21233b1855
iamnirmank <nirmankhadka.aifiverse@gmail.com>
2024-07-21 19:16:44 +0545

    Db

--- paAI/settings.py
+++ paAI/settings.py
@@ -99,11 +99,11 @@ WSGI_APPLICATION = 'paAI.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('DB_NAME'),
-        'USER': os.getenv('DB_USER'),
-        'PASSWORD': os.getenv('DB_PASSWORD'),
-        'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT'),
+        'NAME': 'postgres',
+        'USER': 'paiaiuser',
+        'PASSWORD': 'Paai2024@',
+        'HOST': 'paaiserver.postgres.database.azure.com',
+        'PORT': '5432',
     }
 }
 
